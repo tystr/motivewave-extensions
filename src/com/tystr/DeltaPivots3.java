be1c22644958d733dc71c0801dc64ada81611d8b
Tyler Stroud <tyler@tylerstroud.com>
2021-08-03 21:32:01 -0400

    delta pivots wip

--- src/com/tystr/DeltaPivots3.java
+++ src/com/tystr/DeltaPivots3.java
@@ -49,14 +49,12 @@ public class DeltaPivots3 extends Study
         grp.addRow(new IntegerDescriptor("Volume", "Volume", 10000, 1, Integer.MAX_VALUE, 1));
         grp.addRow(new IntegerDescriptor("WindowSize", "Window Size", 10, 1, Integer.MAX_VALUE, 1));
 
-
         List<NVP> timeframes = List.of(
                 new NVP("Daily", "Daily"),
                 new NVP("Weekly", "Weekly")
         );
 
         grp.addRow(new DiscreteDescriptor("Timeframe", "Timeframe", "Daily", timeframes));
-
 
         grp.addRow(new BooleanDescriptor("HighlightBars", "Highlight Bars", true));
         sd.addQuickSettings("SmoothingBars", "PivotLine", "HighExtensionLine", "LowExtensionLine", "ValueAreaPercent");
@@ -174,26 +172,22 @@ public class DeltaPivots3 extends Study
             this.series = series;
             this.nextIndex = startIndex;
             this.deltaByPrice = new TreeMap<>();
-            nextEnd = getEndForTimeframe(getSettings().getString("Timeframe"), series.getStartTime(startIndex));
-            nextStart = series.getInstrument().getStartOfDay(series.getStartTime(startIndex) + Util.MILLIS_IN_DAY, rth);
-
             this.defaults = defaults;
 
             Instrument instrument = series.getInstrument();
-            // once we pass this time, session windows are recalculated
             this.tradingDayStart = instrument.getStartOfDay(series.getStartTime(nextIndex), false);
             this.nextTradingDayStart = Util.getStartOfNextDay(series.getStartTime(nextIndex), series.getInstrument(), false); // globex open of next day
 
             // compute session windows for the day
-            // rth session:
-            rthStart = instrument.getStartOfDay(series.getStartTime(nextIndex), true) + (4 * Util.MILLIS_IN_HOUR);;
-            rthEnd = rthStart + (2 * Util.MILLIS_IN_HOUR) + (20 * Util.MILLIS_IN_MINUTE);//instrument.getEndOfDay(tickTime, true) - Util.MILLIS_IN_HOUR - (10 * Util.MILLIS_IN_MINUTE);
             // gbx session:
-            gbxStart = instrument.getStartOfEveningSession(series.getStartTime(nextIndex)) + Util.MILLIS_IN_HOUR;
-            gbxEnd = gbxStart + (9 * Util.MILLIS_IN_HOUR) + (30 * Util.MILLIS_IN_MINUTE);
+            gbxStart = this.tradingDayStart;
+            gbxEnd = gbxStart + (9 * Util.MILLIS_IN_HOUR) + (25 * Util.MILLIS_IN_MINUTE);
             // euro session:
-            euroStart = instrument.getStartOfEveningSession(series.getStartTime(nextIndex)) + (10 * Util.MILLIS_IN_HOUR) + (31 * Util.MILLIS_IN_MINUTE);
-            euroEnd = euroStart + (6 * Util.MILLIS_IN_HOUR) - Util.MILLIS_IN_HOUR; // this should be 9:30am est
+            euroStart = this.tradingDayStart +  (9 * Util.MILLIS_IN_HOUR) + (31 * Util.MILLIS_IN_MINUTE);
+            euroEnd = euroStart + (5 * Util.MILLIS_IN_HOUR) ; // this should be 9:30am est
+            // rth session:
+            rthStart = this.tradingDayStart + (19 * Util.MILLIS_IN_HOUR) + (30 * Util.MILLIS_IN_MINUTE);
+            rthEnd = rthStart + (2 * Util.MILLIS_IN_HOUR) + (20 * Util.MILLIS_IN_MINUTE);//instrument.getEndOfDay(tickTime, true) - Util.MILLIS_IN_HOUR - (10 * Util.MILLIS_IN_MINUTE);
         }
 
         private void calculateRollingWindow() {
@@ -210,7 +204,7 @@ public class DeltaPivots3 extends Study
             for (int i = nextIndex; i >= windowStartIndex; i--) {
                 windowSum += series.getInt(i, Values.DELTA);
             }
-            if (Math.abs(windowSum) > Math.abs(maxDeltaWindowSum)) {
+            if (Math.abs(windowSum) >= Math.abs(maxDeltaWindowSum)) {
                 maxDeltaWindowSum = windowSum;
                 maxDeltaWindowStartIndex = windowStartIndex;
             }
@@ -219,60 +213,46 @@ public class DeltaPivots3 extends Study
         }
 
         private void colorBars() {
-
+            Color barColor = maxDeltaWindowSum > 0 ? defaults.getGreen() : defaults.getRed();
+            for (int i = maxDeltaWindowStartIndex; i < (maxDeltaWindowStartIndex + getSettings().getInteger("WindowSize")); i++) {
+//                series.setPriceBarColor(i, getSettings().getColor("WindowBarColor"));
+                series.setPriceBarColor(i, barColor);
+            }
         }
-
 
         public void onTick(Tick tick) {
             if (series.isComplete(nextIndex)) {
                 return; // nothing to do if this index is complete
             }
             long tickTime = tick.getTime();
-            ZonedDateTime tickDT = ZonedDateTime.ofInstant(Instant.ofEpochMilli(tickTime), ZoneId.of("UTC"));
             Instrument instrument = series.getInstrument();
 
-            boolean isWeekend = false;
             // if new day, recalculate sessions
             if (tickTime > nextTradingDayStart) {
+                tradingDayStart = nextTradingDayStart;
                 nextTradingDayStart = Util.getStartOfNextDay(tickTime, instrument, false);
 
-                if (nextTradingDayStart == Util.getStartOfNextWeek(tickTime, instrument, false)) {
-                    isWeekend = true;
-                    debug("WEEKEND");
-                    // next session will be globex
-
-                }
                 debug("Setting next trading day start: " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(nextTradingDayStart), ZoneId.of("UTC")));
                 // rth session:
-                rthStart = instrument.getStartOfDay(tickTime, true) + (4 * Util.MILLIS_IN_HOUR);
-                rthEnd = rthStart + (2 * Util.MILLIS_IN_HOUR) + (20 * Util.MILLIS_IN_MINUTE);//instrument.getEndOfDay(tickTime, true) - Util.MILLIS_IN_HOUR - (10 * Util.MILLIS_IN_MINUTE);
-                // gbx session:
-                if (isWeekend) {
-                    gbxStart = instrument.getStartOfEveningSession(tickTime) + Util.MILLIS_IN_HOUR;
-                    gbxEnd = gbxStart + (9 * Util.MILLIS_IN_HOUR) + (20 * Util.MILLIS_IN_MINUTE);
-                } else {
-
-                    gbxEnd = gbxStart + (9 * Util.MILLIS_IN_HOUR) + (20 * Util.MILLIS_IN_MINUTE);
-                }
+                gbxStart = this.tradingDayStart;
+                gbxEnd = gbxStart + (9 * Util.MILLIS_IN_HOUR) + (25 * Util.MILLIS_IN_MINUTE);
                 // euro session:
-                euroStart = instrument.getStartOfEveningSession(tickTime) + (10 * Util.MILLIS_IN_HOUR) + (30 * Util.MILLIS_IN_MINUTE);
-                euroEnd = euroStart + (6 * Util.MILLIS_IN_HOUR); // this should be 9:30am est
-                debug("RTH: " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(rthStart), ZoneId.of("UTC")) + " -> " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(rthEnd), ZoneId.of("UTC")));
+                euroStart = this.tradingDayStart +  (9 * Util.MILLIS_IN_HOUR) + (31 * Util.MILLIS_IN_MINUTE);
+                euroEnd = euroStart + (5 * Util.MILLIS_IN_HOUR); // this should be 9:30am est
+                // rth session:
+                rthStart = this.tradingDayStart + (19 * Util.MILLIS_IN_HOUR) + (30 * Util.MILLIS_IN_MINUTE);
+                rthEnd = rthStart + (2 * Util.MILLIS_IN_HOUR) + (20 * Util.MILLIS_IN_MINUTE);//instrument.getEndOfDay(tickTime, true) - Util.MILLIS_IN_HOUR - (10 * Util.MILLIS_IN_MINUTE);
+                // euro session:
                 debug("GBX: " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(gbxStart), ZoneId.of("UTC")) + " -> " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(gbxEnd), ZoneId.of("UTC")));
                 debug("EURO: " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(euroStart), ZoneId.of("UTC")) + " -> " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(euroEnd), ZoneId.of("UTC")));
+                debug("RTH: " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(rthStart), ZoneId.of("UTC")) + " -> " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(rthEnd), ZoneId.of("UTC")));
             }
 
             boolean insideWindow = false;
             long currentStart = 0;
             long currentEnd = 0;
-            if (tickTime >= rthStart && tickTime < rthEnd) {
-                insideWindow = true;
-                currentSession = "RTH";
-                currentStart = rthStart;
-                currentEnd = rthEnd;
-                nextEnd = currentEnd;
-                nextStart = currentStart;
-            } else if (tickTime >= gbxStart && tickTime < gbxEnd) {
+
+            if (tickTime >= gbxStart && tickTime < gbxEnd) {
                 insideWindow = true;
                 currentSession = "GBX";
                 currentStart = gbxStart;
@@ -286,27 +266,23 @@ public class DeltaPivots3 extends Study
                 currentEnd = euroEnd;
                 nextEnd = currentEnd;
                 nextStart = currentStart;
-            } else {
-//                debug("not inside window: last windo start at " +
-//                ZonedDateTime.ofInstant(Instant.ofEpochMilli(nextStart), ZoneId.of("UTC")) +
-//                        " and ending at " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(nextEnd), ZoneId.of("UTC"))
-//                );
-                // not inside window, nothing to do
+            } else if (tickTime >= rthStart && tickTime < rthEnd) {
+                insideWindow = true;
+                currentSession = "RTH";
+                currentStart = rthStart;
+                currentEnd = rthEnd;
+                nextEnd = currentEnd;
+                nextStart = currentStart;
             }
-//            nextEnd = currentEnd;
-//            nextStart = currentStart;// + (Util.MILLIS_IN_DAY);
-//            ZonedDateTime tickDateTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(tickTime), ZoneId.of("UTC"));
 
-            if (tickTime > series.getEndTime(nextIndex) && insideWindow){
+            if (tickTime >= series.getEndTime(nextIndex)){
                 // Bar close inside session window, do rolling window calc if we have delta
-                if (!deltaByPrice.isEmpty()) {
+                if (insideWindow && !deltaByPrice.isEmpty()) {
                     calculateRollingWindow();
                 }
                 nextIndex++;
             }
 
-            // reset if after end of timeframe (daily, weekly, etc)
-//            if (calculating && tickTime > nextEnd) {
             if (calculating && !insideWindow) {
                 calculating = false;
 
@@ -319,12 +295,10 @@ public class DeltaPivots3 extends Study
 
                 // color bars
                 debug("max delta for session " + currentSession + " starts at index " + maxDeltaWindowStartIndex);
-                for (int i = maxDeltaWindowStartIndex; i < (maxDeltaWindowStartIndex + getSettings().getInteger("WindowSize")); i++) {
-                    series.setPriceBarColor(i, getSettings().getColor("WindowBarColor"));
-                }
-                // draw pivots
+                debug("maxDeltaWindowSTartTime:" + ZonedDateTime.ofInstant(Instant.ofEpochMilli(series.getStartTime(maxDeltaWindowStartIndex)), ZoneId.of("UTC")));
+                colorBars();
 
-                // update nextEnd with the end time of the next session/period
+                // draw pivots
 
                 debug("Completed Window ending at " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(nextEnd), ZoneId.of("UTC")));
 //                debug("Completed Window ending at " + nextEnd);
@@ -345,13 +319,6 @@ public class DeltaPivots3 extends Study
                         ZonedDateTime.ofInstant(Instant.ofEpochMilli(nextStart), ZoneId.of("UTC")) +
                         " and ending at " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(nextEnd), ZoneId.of("UTC"))
                 );
-//                debug ("New window starting at " +
-//                        nextStart +
-//                        " and ending at " + nextEnd);
-
-
-//                nextEnd = getEndForTimeframe(getSettings().getString("Timeframe"), tick.getTime());
-//                debug("nextEnd: " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(nextEnd), ZoneId.of("UTC")));
 
                 // reset
                 maxDeltaWindowStartIndex = 0;
@@ -360,10 +327,12 @@ public class DeltaPivots3 extends Study
             }
 
             // if we are after or equal to next start and haven't begun calculating yet, set next start
-//            if (!calculating && tick.getTime() >= nextStart) {
             if (insideWindow) {
                 if (!calculating) {
-//                nextStart = series.getInstrument().getStartOfDay(tick.getTime(), rth) + (24 * Util.MILLIS_IN_HOUR);
+                    // make sure we're reset
+                    maxDeltaWindowStartIndex = 0;
+                    maxDeltaWindowSum = 0;
+                    deltaByPrice.clear();
                     // starting a new session window, mark the start for debugging
                     Marker startArrow = new Marker(new Coordinate(series.getStartTime(series.findIndex(tick.getTime())), series.getLow(series.findIndex(tick.getTime())) - 2), Enums.MarkerType.TRIANGLE);
                     startArrow.setSize(Enums.Size.LARGE);
@@ -384,25 +353,6 @@ public class DeltaPivots3 extends Study
                     delta -= tick.getVolume();
                     deltaByPrice.put(tick.getBidPrice(), delta);
                 }
-            }
-        }
-
-        private long getEndForTimeframe(String timeframe, long time) {
-            Instrument instrument = series.getInstrument();
-
-            long rthStart = instrument.getStartOfDay(time, rth);
-            long rthEnd = instrument.getEndOfDay(time, rth);
-
-            switch (timeframe) {
-                case "Daily":
-                    debug("using daily");
-                    return series.getInstrument().getEndOfDay(time, rth) - (2 * Util.MILLIS_IN_HOUR) - (Util.MILLIS_IN_MINUTE * 10);
-
-                case "Weekly":
-                    debug("using weekly");
-                    return series.getInstrument().getEndOfWeek(time, rth);
-                default:
-                    throw new RuntimeException("Timeframe must be one of \"Daily\" or \"Weekly\", received \"" + timeframe + "\".");
             }
         }
     }
