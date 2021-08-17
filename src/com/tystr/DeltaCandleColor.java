168bb21ee7631939ffacdd14f4311b6b38c8616d
Tyler Stroud <tyler@tylerstroud.com>
2021-08-17 17:37:37 -0400

    more updates to delta candle color

--- src/com/tystr/DeltaCandleColor.java
+++ src/com/tystr/DeltaCandleColor.java
@@ -2,6 +2,7 @@ package com.tystr;
 
 import com.motivewave.platform.sdk.common.*;
 import com.motivewave.platform.sdk.common.desc.*;
+import com.motivewave.platform.sdk.study.RuntimeDescriptor;
 import com.motivewave.platform.sdk.study.Study;
 import com.motivewave.platform.sdk.study.StudyHeader;
 import com.tystr.delta.DeltaBar;
@@ -21,7 +22,7 @@ import com.tystr.delta.DeltaBar;
 )
 public class DeltaCandleColor extends Study
 {
-    enum Values { DELTA };
+    enum Values { DELTA, DELTA_PERCENT};
 
     @Override
     public void initialize(Defaults defaults)
@@ -29,12 +30,33 @@ public class DeltaCandleColor extends Study
         var sd = createSD();
         var tab = sd.addTab("General");
 
-        var grp = tab.addGroup("Display");
-        grp.addRow(new IntegerDescriptor("PositiveDeltaThreshold", "Positive Threshold %", 20, 0, 100, 1));
-        grp.addRow(new ColorDescriptor("PositiveDeltaColor", "Positive Delta Candle Color", defaults.getGreen()));
-        grp.addRow(new IntegerDescriptor("NegativeDeltaThreshold", "Negative Threshold %", -20, -100, 0, 1));
-        grp.addRow(new ColorDescriptor("NegativeDeltaColor", "Negative Delta Candle Color", defaults.getRed()));
-        grp.addRow(new ColorDescriptor("NeutralDeltaColor", "Neutral Delta Candle Color", defaults.getOrange()));
+        SettingGroup thresholdGroup = tab.addGroup("Thresholds");
+        thresholdGroup.addRow(new IntegerDescriptor("PositiveDeltaThreshold", "Positive Threshold %", 20, 0, 100, 1));
+        thresholdGroup.addRow(new IntegerDescriptor("NegativeDeltaThreshold", "Negative Threshold %", -20, -100, 0, 1));
+        thresholdGroup.addRow(
+                new IntegerDescriptor("NeutralDeltaLowThreshold", "Neutral Low Threshold %", -5, -100, 0, 1),
+                new IntegerDescriptor("NeutralDeltaHighThreshold", "Neutral High Threshold %", 5, 0, 100, 1)
+        );
+
+        SettingGroup colorGroup = tab.addGroup("Colors");
+        colorGroup.addRow(new ColorDescriptor("PositiveDeltaColor", "Positive Delta Candle Color", defaults.getGreen().darker()));
+        colorGroup.addRow(new ColorDescriptor("NegativeDeltaColor", "Negative Delta Candle Color", defaults.getRed()));
+        colorGroup.addRow(new ColorDescriptor("NeutralDeltaColor", "Neutral Delta Candle Color", defaults.getOrange()));
+
+        sd.addQuickSettings(
+                "PositiveDeltaThreshold",
+                "NegativeDeltaThreshold",
+                "NeutralDeltaLowThreshold",
+                "NeutralDeltaHighThreshold",
+                "PositiveDeltaColor",
+                "NegativeDeltaColor",
+                "NeutralDeltaColor"
+        );
+
+        RuntimeDescriptor rd = getRuntimeDescriptor();
+        rd.exportValue(new ValueDescriptor(Values.DELTA, "Delta", null));
+        rd.exportValue(new ValueDescriptor(Values.DELTA_PERCENT, "Delta %", null));
+
     }
 
     @Override
@@ -42,13 +64,7 @@ public class DeltaCandleColor extends Study
         DataSeries series = ctx.getDataSeries();
         Instrument instrument = series.getInstrument();
 
-        int maxDays = 10; // @todo configure this
         int startIndex = 1;
-        long threshold = instrument.getStartOfDay(series.getStartTime(), ctx.isRTH()) - ((maxDays+1) * Util.MILLIS_IN_DAY);
-        for (int i = series.size()-1; i > 0; i--) {
-            startIndex = i;
-            if (series.getStartTime(i) < threshold) break;
-        }
         TickOperation calculator = new DeltaCalculator(startIndex, series);
         instrument.forEachTick(series.getStartTime(startIndex), ctx.getCurrentTime() + Util.MILLIS_IN_MINUTE, ctx.isRTH(), calculator);
     }
@@ -58,6 +74,12 @@ public class DeltaCandleColor extends Study
     }
     private float getNegativeDeltaThreshold() {
         return (float) getSettings().getInteger("NegativeDeltaThreshold") / 100;
+    }
+    private float getNeutralDeltaLowThreshold() {
+        return (float) getSettings().getInteger("NeutralDeltaLowThreshold") / 100;
+    }
+    private float getNeutralDeltaHighThreshold() {
+        return (float) getSettings().getInteger("NeutralDeltaHighThreshold") / 100;
     }
 
     class DeltaCalculator implements TickOperation {
@@ -78,12 +100,13 @@ public class DeltaCandleColor extends Study
 
             if (tick.getTime() > series.getEndTime(nextIndex)) { // Bar is complete, set color and reset delta
                 if (deltaBar.isEmpty()) return;
-                debug("delta percent (" + deltaBar.getVolume() + " / " + deltaBar.getDelta() + "): " + deltaBar.getDeltaPercent());
-                if (deltaBar.getDeltaPercent() > getPositiveDeltaThreshold()) {
+
+                float deltaPercent = deltaBar.getDeltaPercent();
+                if (deltaPercent > getPositiveDeltaThreshold()) {
                     series.setPriceBarColor(nextIndex, getSettings().getColor("PositiveDeltaColor"));
-                } else if (deltaBar.getDeltaPercent() < getNegativeDeltaThreshold()) {
+                } else if (deltaPercent < getNegativeDeltaThreshold()) {
                     series.setPriceBarColor(nextIndex, getSettings().getColor("NegativeDeltaColor"));
-                } else {
+                } else if (deltaPercent > getNeutralDeltaLowThreshold() && deltaPercent < getNeutralDeltaHighThreshold()) {
                     series.setPriceBarColor(nextIndex, getSettings().getColor("NeutralDeltaColor"));
                 }
                 notifyRedraw();
@@ -103,6 +126,7 @@ public class DeltaCandleColor extends Study
                 deltaBar.addVolumeAtBid(tick.getBidPrice(), tick.getVolume());
             }
             series.setInt(nextIndex, Values.DELTA, deltaBar.getDelta());
+            series.setFloat(nextIndex, Values.DELTA_PERCENT, deltaBar.getDeltaPercent());
         }
     }
 }
