ff72c32a758637ec63f936c337a1700280c5885b
Tyler Stroud <tyler@tylerstroud.com>
2022-01-19 08:58:43 -0500

    write levels to csv

--- src/com/tystr/study/VolumePivots.java
+++ src/com/tystr/study/VolumePivots.java
@@ -6,9 +6,15 @@ import com.motivewave.platform.sdk.draw.Line;
 import com.motivewave.platform.sdk.study.Plot;
 import com.motivewave.platform.sdk.study.Study;
 import com.motivewave.platform.sdk.study.StudyHeader;
+import com.tystr.DeltaPivots3;
 import com.tystr.VolumeProfile;
 import com.tystr.study.overlay.PivotSet;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 @StudyHeader(
@@ -19,15 +25,17 @@ import java.util.*;
         desc="This study plots pivots based on prior volume profile data",
         menu="Tystr",
         overlay=true,
-        studyOverlay=true,
-        requiresVolume = true
+        studyOverlay=false,
+        requiresVolume = true,
+        requiresBarUpdates=true
+
 )
 public class VolumePivots extends Study
 {
     enum Values { VAH, VAL, VAH_1, VAH_2, VAL_1, VAL_2, VA_PIVOT, TIMEFRAME, VOLUME_BY_PRICE};
     enum Intervals {DAILY, WEEKLY}
 
-    TickOperation calculator;
+    VPCalculator calculator;
     private boolean isCalculating = false;
     private boolean calculated = false;
 
@@ -37,6 +45,7 @@ public class VolumePivots extends Study
     private final String PIVOT = "pivot";
     private final String EXTENSIONS_ABOVE = "extensionAbove";
     private final String EXTENSIONS_BELOW = "extensionBelow";
+    private final String USE_SERIES_BS = "useSeriesBS";
 
     private final List<Line> lines = new LinkedList<>();
 
@@ -58,6 +67,8 @@ public class VolumePivots extends Study
         grp.addRow(new IntegerDescriptor(NUM_PRINTS, "Number of Prints", 10, 1, 9999, 1));
         grp.addRow(new DiscreteDescriptor(TIMEFRAME, "Interval", "Daily", timeframes));
         grp.addRow(new BooleanDescriptor(RTH_DATA, "RTH Data", true));
+
+        grp.addRow(new BarSizeDescriptor(Inputs.BARSIZE, "Bar Size", BarSize.getBarSize(1440)), new BooleanDescriptor(USE_SERIES_BS, "Use Series Bar Size", false, false));
 
         // Pivots
         PathDescriptor pivotPathDescriptor = new PathDescriptor(PIVOT, "Pivot", defaults.getYellowLine(), 2.0f, null, true, true, true);
@@ -83,9 +94,18 @@ public class VolumePivots extends Study
 //        sd.addQuickSettings(PIVOT, EXTENSIONS_ABOVE, EXTENSIONS_BELOW);
         sd.addQuickSettings(Inputs.PATH,Inputs.PATH2, Inputs.PATH3);
         sd.addQuickSettings(PivotSet.SHOW_PRICES);
+        sd.addDependency(new EnabledDependency(false, USE_SERIES_BS, Inputs.BARSIZE));
 
-        clearFigures();
-        lines.clear();
+
+
+        grp = tab.addGroup("CSV Output");
+        grp.addRow(new BooleanDescriptor("WriteCsv", "Write Levels to CSV", true));
+        grp.addRow(new StringDescriptor("CsvFilePath", "Path (filename will be SDP_<SYMBOL>.csv):", ""));
+        sd.addDependency(new EnabledDependency("WriteCsv", "CsvFilePath"));
+
+//        clearFigures();
+        setMinBars(2);
+//        lines.clear();
     }
 
     @Override
@@ -94,10 +114,14 @@ public class VolumePivots extends Study
         System.err.println("Clearing state...");
         super.clearState();
         lines.clear();
+        calculated = false;
     }
 
     @Override
     protected void calculateValues(DataContext ctx) {
+//        DataSeries series11 = ctx.getDataSeries();
+//        BarSize barSize = getSettings().getBarSize(Inputs.BARSIZE);
+
         DataSeries series = ctx.getDataSeries();
         if (series.size() == 0 || isCalculating) return;
         Instrument instrument = series.getInstrument();
@@ -119,6 +143,12 @@ public class VolumePivots extends Study
         isCalculating = false;
         calculated = true;
 
+        notifyRedraw();
+
+        if (getSettings().getBoolean("WriteCsv")) {
+            writeFile(calculator.getLastVolumeProfile(), getSettings().getString("CsvFilePath"));
+        }
+
 //        clearFigures(Plot.PRICE);
 //        removeFigures(lines);
 //        for (Line l : lines) {
@@ -128,6 +158,139 @@ public class VolumePivots extends Study
 //        }
 
 //        Util.schedule(() -> calculate(this, finalStart, isRTH));
+    }
+
+    private void appendRow(StringBuilder builder, String symbol, float price, String note, String fColor, String bColor, String diameter) {
+        builder.append(symbol).append(",")
+                .append(price).append(",")
+                .append(note).append(",")
+                .append(fColor).append(",")
+                .append(bColor).append(",")
+                .append(diameter)
+                .append("\n");
+    }
+
+
+    /**
+     * Writes pivot levels to a file for use with cloud levels study
+     * @param volumeProfile
+     * @param filePath
+     */
+    protected void writeFile(VolumeProfile volumeProfile, String filePath) {
+        Path path = Paths.get(filePath);
+        if (!Files.exists(path) || !Files.isWritable(path)) {
+            System.err.println("Unable to write to " + path.toString());
+            return;
+        }
+
+        float above1Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f);
+        float above2Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 2);
+        float above3Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 3);
+        float above4Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 4);
+        float below1Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f);
+        float below2Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f * 2);
+        float below3Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f * 3);
+        float below4Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f * 4);
+
+        StringBuilder content = new StringBuilder()
+                .append("Symbol,Price Level,Note,Foreground Color,Background Color,Diameter\n");
+
+        content.append(volumeProfile.getInstrumentSymbol()).append(",")
+                .append(volumeProfile.getValueAreaMid()).append(",")
+                .append("VP").append(",")
+                .append("#ffffff").append(",")
+                .append("ffa700").append(",")
+                .append("2")
+                .append("\n");
+
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                above1Price,
+                "VP 1A",
+                "#ffffff",
+                "#5757ff",
+                "2"
+        );
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                above2Price,
+                "VP 2A",
+                "#ffffff",
+                "#5757ff",
+                "2"
+        );
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                above3Price,
+                "VP 3A",
+                "#ffffff",
+                "#5757ff",
+                "2"
+        );
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                above4Price,
+                "VP 4A",
+                "#ffffff",
+                "#5757ff",
+                "2"
+        );
+
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                below1Price,
+                "SDP 1B",
+                "#ffffff",
+                "#b20000",
+                "2"
+        );
+
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                below2Price,
+                "SDP 2B",
+                "#ffffff",
+                "#b20000",
+                "2"
+        );
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                below3Price,
+                "SDP 3B",
+                "#ffffff",
+                "#b20000",
+                "2"
+        );
+        this.appendRow(
+                content,
+                volumeProfile.getInstrumentSymbol(),
+                below4Price,
+                "SDP 4B",
+                "#ffffff",
+                "#b20000",
+                "2"
+        );
+
+
+        System.err.println("\nFILE CONTENTS\n");
+        System.err.println(content.toString() + "\n");
+        System.err.println("Using file path " + path.toString());
+        String fileName = path.toString() + "/VP_" + volumeProfile.getInstrumentSymbol() + ".csv";
+
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(fileName), StandardCharsets.UTF_8))) {
+            writer.write(content.toString());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        System.err.println("levels written to " + fileName);
     }
 
 //    protected void calculate(VolumePivots study, long start, boolean isRTH) {
@@ -157,6 +320,18 @@ public class VolumePivots extends Study
 //        calculator.onTick(tick);
 //    }
 
+    static class VolumeProfile extends com.tystr.VolumeProfile {
+        private String instrumentSymbol;
+
+        public void setInstrumentSymbol(String instrumentSymbol) {
+            this.instrumentSymbol = instrumentSymbol;
+        }
+
+        public String getInstrumentSymbol() {
+            return instrumentSymbol;
+        }
+    }
+
     static class VolumeByPrice extends TreeMap<Float, Integer> {
         public void addVolumeAtPrice(float price, int volume) {
             int volumeAtPrice = getOrDefault(price, 0);
@@ -169,6 +344,7 @@ public class VolumePivots extends Study
         private int nextIndex;
         private final boolean rth;
         private final VolumeProfile volumeProfile;
+        private VolumePivots.VolumeProfile lastVolumeProfile;
         private VolumeByPrice volumeByPrice = new VolumeByPrice();
 
         private long nextEnd;
@@ -181,6 +357,9 @@ public class VolumePivots extends Study
             nextEnd = getEndForTimeframe(getSettings().getString("Timeframe"), series.getStartTime(startIndex));
         }
 
+        public VolumeProfile getLastVolumeProfile() {
+            return lastVolumeProfile;
+        }
         public void onTick(Tick tick) {
             if (series.isComplete(series.findIndex(tick.getTime()))) {
 //                System.err.println("Index " + series.findIndex(tick.getTime()) + "|" + nextIndex + " is complete...");
@@ -204,65 +383,24 @@ public class VolumePivots extends Study
                 nextEnd = getEndForTimeframe(getSettings().getString("Timeframe"), tick.getTime());
                 Coordinate end = new Coordinate(nextEnd, volumeProfile.getValueAreaMid());
 
-//                System.err.println("start: " + start);
-//                System.err.println("end: " + end);
-//                System.err.println("--");
-                // pivot
-                Line pivot = new Line(start, end, getSettings().getPath(PIVOT));
-
-                // extensions
-                PathInfo extensionsAbove = getSettings().getPath(EXTENSIONS_ABOVE);
-                PathInfo extensionsBelow = getSettings().getPath(EXTENSIONS_BELOW);
-
                 float above1Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f);
-                Line above1 = new Line(new Coordinate(s, above1Price), new Coordinate(nextEnd, above1Price), extensionsAbove);
-
-                float above2Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 2); Line above2 = new Line(new Coordinate(s, above2Price), new Coordinate(nextEnd, above2Price), extensionsAbove); float above3Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 3);
-                Line above3 = new Line(new Coordinate(s, above3Price), new Coordinate(nextEnd, above3Price), extensionsAbove);
-
+                float above2Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 2);
+                float above3Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 3);
                 float above4Price = volumeProfile.getValueAreaHigh() + (volumeProfile.getValueAreaBreadth() * 0.50f * 4);
-                Line above4 = new Line(new Coordinate(s, above4Price), new Coordinate(nextEnd, above4Price), extensionsAbove);
-
                 float below1Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f);
-                Line below1 = new Line(new Coordinate(s, below1Price), new Coordinate(nextEnd, below1Price), extensionsBelow);
-
                 float below2Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f * 2);
-                Line below2 = new Line(new Coordinate(s, below2Price), new Coordinate(nextEnd, below2Price), extensionsBelow);
-
                 float below3Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f * 3);
-                Line below3 = new Line(new Coordinate(s, below3Price), new Coordinate(nextEnd, below3Price), extensionsBelow);
-
                 float below4Price = volumeProfile.getValueAreaLow() - (volumeProfile.getValueAreaBreadth() * 0.50f * 4);
-                Line below4 = new Line(new Coordinate(s, below4Price), new Coordinate(nextEnd, below4Price), extensionsBelow);
 
-
-                PivotSet pivotSet = new PivotSet(s, nextEnd, volumeProfile.getValueAreaMid(), above1Price, above2Price, below1Price, below2Price);
+                PivotSet pivotSet = new PivotSet(s, nextEnd, volumeProfile.getValueAreaMid(), above1Price, above2Price, above3Price, above4Price, below1Price, below2Price, below3Price, below4Price);
                 pivotSet.P = "Pivot";
                 beginFigureUpdate();
                 addFigure(Plot.PRICE, pivotSet);
                 endFigureUpdate();
-                notifyRedraw();
+//                notifyRedraw();
 
-//                addFigure(Plot.PRICE, pivot);
-//                addFigure(Plot.PRICE, above1);
-//                addFigure(Plot.PRICE, above2);
-//                addFigure(Plot.PRICE, above3);
-//                addFigure(Plot.PRICE, above4);
-//                addFigure(Plot.PRICE, below1);
-//                addFigure(Plot.PRICE, below2);
-//                addFigure(Plot.PRICE, below3);
-//                addFigure(Plot.PRICE, below4);
-//
-//                lines.add(pivot);
-//                lines.add(above1);
-//                lines.add(above2);
-//                lines.add(above3);
-//                lines.add(above4);
-//                lines.add(below1);
-//                lines.add(below2);
-//                lines.add(below3);
-//                lines.add(below4);
-
+                lastVolumeProfile = volumeProfile;
+                lastVolumeProfile.setInstrumentSymbol(series.getInstrument().getSymbol());
                 volumeProfile.clear();
             }
         }
